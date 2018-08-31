d8bbf5ba757387a08444331ce08b380a05ec0265
Arpit Choudhary <arpitkumar147@gmail.com>
2018-08-31 23:37:18 +0530

    tabs to spaces

--- pycodechef/client.py
+++ pycodechef/client.py
@@ -1,63 +1,58 @@
 """
 This is python wrapper for Codechef API
 """
-import json
 import requests
 
 
 class Codechef(object):
-	"""A codechef api wrapper"""
-	
-	__attrs__ = ['client_id', 'client_secret', 'access_token']
+    """A codechef api wrapper"""
 
-	def __init__(self, id, secret):
-		self.client_id = id
-		self.client_secret = secret
-		headers = {
-		    'content-Type': 'application/json',
-		}
+    __attrs__ = ['client_id', 'client_secret', 'access_token']
 
-		data = '{{"grant_type":"client_credentials" , "scope":"public", "client_id":"{}","client_secret":"{}"}}'.format(id, secret)
+    def __init__(self, id, secret):
+        self.client_id = id
+        self.client_secret = secret
+        headers = {
+            'content-Type': 'application/json',
+        }
 
-		response = 
-		response = response.json()
+        data = '{{"grant_type":"client_credentials" , "scope":"public", "client_id":"{}","client_secret":"{}"}}'.format(id, secret)
 
-		if response['status'] == "OK":
-			print('Fetching access token...')
-			self.access_token = response['result']['data']['access_token']
-		else:
-			print('Error', response)
+        response = requests.post('https://api.codechef.com/oauth/token', headers=headers, data=data)
+        response = response.json()
 
-	def _refresh(self):
-		headers = {
-		    'content-Type': 'application/json',
-		}
-		data = '{{"grant_type":"refresh_token" , "refresh_token":"{}", "client_id":"{}","client_secret":"{}"}}'.format(self.access_token, self.client_id, self.client_secret)
-		response = requests.post('https://api.codechef.com/oauth/token', headers=headers, data=data)
-		print('Token successfully refreshed!!')
+        if response['status'] == "OK":
+            print('Fetching access token...')
+            self.access_token = response['result']['data']['access_token']
+        else:
+            print('Error', response)
 
-	def _GET(self, url):
-		token = "Bearer {}".format(self.access_token)
-		headers = {
-		    'Accept': 'application/json',
-	    	'Authorization': token,
-		}
-		response = requests.get(url, headers=headers)
+    def _refresh(self):
+        headers = {
+            'content-Type': 'application/json',
+        }
+        data = '{{"grant_type":"refresh_token" , "refresh_token":"{}", "client_id":"{}","client_secret":"{}"}}'.format(self.access_token, self.client_id, self.client_secret)
 
-		if response.status_code != 200:
-			error = 'HTTPError: {}'.format(response.status_code)
-			return {'success': False, 'error': error}
-		try:
-			return response.json()
-		except ValueError as err:
-			return {'success': False, 'error': err}
+    def _GET(self, url):
+        token = "Bearer {}".format(self.access_token)
+        headers = {
+            'Accept': 'application/json',
+            'Authorization': token,
+        }
+        response = requests.get(url, headers=headers)
 
+        if response.status_code != 200:
+            error = 'HTTPError: {}'.format(response.status_code)
+            return {'success': False, 'error': error}
+        try:
+            return response.json()
+        except ValueError as err:
+            return {'success': False, 'error': err}
 
-	def get_contest_problem(self, contest_code, problem_code):
+    def get_contest_problem(self, contest_code, problem_code):
 
-		url = 'https://api.codechef.com/contests/'
-		path = url + contest_code + '/problems/' + problem_code
-		response = self._GET(path)
+        url = 'https://api.codechef.com/contests/'
+        path = url + contest_code + '/problems/' + problem_code
+        response = self._GET(path)
 
-		return response
-
+        return response
